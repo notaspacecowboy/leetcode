12b68fb1fb73f4358fd88dfe6f95210e3f17a132
Pakhooo <60165135+ZackYang-ops@users.noreply.github.com>
2020-09-24 00:52:19 -0400

    Update LongestPalindromicSubstring.cpp

--- Solutions/C++/0005-LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ Solutions/C++/0005-LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -1,44 +1,41 @@
 #include <string>
 using namespace std;
 
-namespace LongestPalindromicSubstring
-{
-    class Solution {
-    public:
-        string longestPalindrome(string s) {
-            if (s == "")
-                return s;
-        	
-            int max = 0;
-        	const int len = s.length();
-            string ans = "";
-      
-            bool dp[1000][1000];
-            bool flag = false;
-        	
-        	for(int i = 0;i < len; i++)
-        	{
-        		for(int j = 0; j <= i; j++)
-        		{
-                    flag = (s[i] == s[j]);
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s == "")
+            return s;
 
-                    if (i == j)
-                        dp[j][i] = true;
-                    else if (i == j + 1)
-                        dp[j][i] = (flag ? true : false);
+        int max = 0;
+        const int len = s.length();
+        string ans = "";
 
-                    else
-                        dp[j][i] = (dp[j + 1][i - 1] && flag);
+        bool dp[1000][1000];
+        bool flag = false;
 
-        			if(dp[j][i] && i - j + 1 > max)
-        			{
-                        max = i - j + 1;
-                        ans = s.substr(j, i - j + 1);
-        			}
-        		}
-        	}
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                flag = (s[i] == s[j]);
 
-            return ans;
+                if (i == j)
+                    dp[j][i] = true;
+                else if (i == j + 1)
+                    dp[j][i] = (flag ? true : false);
+
+                else
+                    dp[j][i] = (dp[j + 1][i - 1] && flag);
+
+                if (dp[j][i] && i - j + 1 > max)
+                {
+                    max = i - j + 1;
+                    ans = s.substr(j, i - j + 1);
+                }
+            }
         }
-    };
-}
+
+        return ans;
+    }
+};
