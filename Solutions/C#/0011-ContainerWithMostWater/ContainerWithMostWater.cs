420b06bf22ff5671b196a9202edafab8fc1e8949
ZackYangOp <60165135+ZackYang-ops@users.noreply.github.com>
2020-10-30 00:23:34 -0400

    add problem 11

---
+++ Solutions/C#/0011-ContainerWithMostWater/ContainerWithMostWater.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int i = 0;
+            int j = height.Length - 1;
+
+            int max = 0;
+            while (i < j)
+            {
+                int currentArea = (j - i) * (height[i] > height[j] ? height[j] : height[i]);
+
+                if (currentArea > max)
+                    max = currentArea;
+
+                if (height[i] >= height[j])
+                    j--;
+                else
+                    i++;
+            }
+
+            return max;
+        }
+    }
+}
