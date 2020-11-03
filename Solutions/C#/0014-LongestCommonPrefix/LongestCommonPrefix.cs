e4a306add4b8b46e6f7f396fe28fb0ed472183ad
ZackYangOp <60165135+ZackYang-ops@users.noreply.github.com>
2020-11-03 02:04:36 -0500

    add 0014-longest common prefix

---
+++ Solutions/C#/0014-LongestCommonPrefix/LongestCommonPrefix.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace LongestCommonPrefix
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return "";
+
+            string result = "";
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (i >= strs[j].Length || strs[j][i] != strs[0][i])
+                        return result;
+                }
+
+                result += strs[0][i];
+            }
+
+            return result;
+        }
+    }
+}
