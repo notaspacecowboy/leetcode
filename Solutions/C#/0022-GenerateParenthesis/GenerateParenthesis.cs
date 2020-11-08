de833b0e4bf38b3d8994ee6e65315d5c5ee9ba57
ZackYangOp <60165135+ZackYang-ops@users.noreply.github.com>
2020-11-08 01:31:31 -0500

    add 0022-Generate Parenthesis

---
+++ Solutions/C#/0022-GenerateParenthesis/GenerateParenthesis.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace GenerateParenthesis
+{
+    public class Solution
+    {
+        public List<string> ans = new List<string>();
+        public IList<string> GenerateParenthesis(int n)
+        {
+            if (n == 0)
+                return ans;
+
+            else
+                FindParenthesis("(", n, 1, 0);
+
+            return ans;
+        }
+
+        public void FindParenthesis(string currentStr, int total, int left, int right)
+        {
+            if (right > left)
+                return;
+
+            if (left == total)
+            {
+                for (int i = right + 1; i <= total; i++)
+                {
+                    currentStr += ')';
+                }
+                ans.Add(currentStr);
+                return;
+            }
+
+            if (right == left)
+            {
+                FindParenthesis(currentStr + '(', total, left + 1, right);
+                return;
+            }
+
+            FindParenthesis(currentStr + '(', total, left + 1, right);
+            FindParenthesis(currentStr + ')', total, left, right + 1);
+            return;
+        }
+    }
+}
