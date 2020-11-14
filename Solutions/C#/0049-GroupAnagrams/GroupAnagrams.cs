e2d0172396c179ee7baaa76392c9583c6e8b1f10
ZackYangOp <60165135+ZackYang-ops@users.noreply.github.com>
2020-11-14 02:55:39 -0500

    add 0049-group anagrams

---
+++ Solutions/C#/0049-GroupAnagrams/GroupAnagrams.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace GroupAnagrams
+{
+    public class Solution
+    {
+        IList<IList<string>> ans = new List<IList<string>>();
+        Dictionary<string, List<int>> positionDic = new Dictionary<string, List<int>>();
+
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            for (int i = 0; i < strs.Length; i++)
+            {
+
+                char[] charArray = strs[i].ToCharArray();
+                Array.Sort(charArray);
+                string tmp = new string(charArray);
+                if (positionDic.ContainsKey(tmp))
+                {
+                    positionDic[tmp].Add(i);
+                }
+                else
+                {
+                    positionDic.Add(tmp, new List<int>() { i });
+                }
+            }
+
+            foreach (var positionList in positionDic.Values)
+            {
+                List<string> tmp = new List<string>();
+                for (int i = 0; i < positionList.Count; i++)
+                {
+                    tmp.Add(strs[positionList[i]]);
+                }
+                ans.Add(tmp);
+            }
+
+            return ans;
+        }
+    }
+}
