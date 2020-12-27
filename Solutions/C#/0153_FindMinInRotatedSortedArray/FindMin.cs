90f7539fae0278726ad1a79beaf401ed784ff552
ZackYangOp <60165135+ZackYang-ops@users.noreply.github.com>
2020-12-27 07:21:40 -0500

    Binary Search

---
+++ Solutions/C#/0153_FindMinInRotatedSortedArray/FindMin.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        int left = 0, right = nums.Length - 1, minn = nums[0], mid;
+        while (left <= right)
+        {
+            mid = (left + right) / 2;
+            if (nums[mid] < minn)
+                minn = nums[mid];
+
+            if (nums[mid] >= nums[0])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return minn;
+    }
+}
