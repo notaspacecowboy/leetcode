c643186a82547c7469916050440b50fd750d8b78
ZackYangOp <60165135+ZackYang-ops@users.noreply.github.com>
2021-01-01 12:11:48 -0500

    Binary Search

---
+++ Solutions/C#/0278_FirstBadVersion/FirstBadVersion.cs
@@ -0,0 +1,22 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) 
+    {
+        if(n == 1)
+            return 1;
+        
+        int left = 1, right = n;
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(IsBadVersion(mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        
+        return right;
+    }
+}
