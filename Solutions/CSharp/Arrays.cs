be1227e4644ed3b5b125a543bdf61aa8876baf92
Zack Yang <zyang464@uwo.ca>
2022-10-03 23:30:28 -0400

    added 0064, 0066, 0067

--- Solutions/CSharp/Arrays.cs
+++ Solutions/CSharp/Arrays.cs
@@ -428,4 +428,31 @@ public class Arrays
     }
 
     #endregion
+
+    #region 0066 - Plus One
+
+    public int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            digits[i] = (digits[i] + 1);
+            if (digits[i] == 10)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                return digits;
+            }
+        }
+
+        int[] results = new int[digits.Length + 1];
+        results[0] = 1;
+        Array.Copy(digits, 0, results, 1, digits.Length);
+        return results;
+
+        return digits;
+    }
+
+    #endregion
 }
--- Solutions/CSharp/DynamicProgramming.cs
+++ Solutions/CSharp/DynamicProgramming.cs
@@ -143,4 +143,31 @@ public class DynamicProgramming
 
     #endregion
 
+    #region 0064 - Minimum Path Sum
+
+    public int MinPathSum(int[][] grid)
+    {
+        int m = grid.Length, n = grid[0].Length;
+        int[,] dp = new int[m, n];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 && j == 0)
+                    dp[i, j] = grid[i][j];
+                else if (i == 0)
+                    dp[i,j] = dp[i, j - 1] + grid[i][j];
+                else if (j == 0)
+                    dp[i,j] = dp[i - 1, j] + grid[i][j];
+                else
+                    dp[i, j] = Math.Min(dp[i - 1, j], dp[i, j - 1]) + grid[i][j];
+            } 
+        }
+
+        return dp[m - 1, n - 1];
+    }
+
+    #endregion
+
 }
