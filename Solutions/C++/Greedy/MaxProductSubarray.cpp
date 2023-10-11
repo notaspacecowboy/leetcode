1fc555da659a63f0c820f8ad56a0e40b12871ae9
Zack Yang <pakho5211@gmail.com>
2023-10-11 00:03:41 -0400

    10/11/2023

---
+++ Solutions/C++/Greedy/MaxProductSubarray.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int globalMax = INT32_MIN;
+        int currentProduct = 1;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            currentProduct *= nums[i];
+            globalMax = currentProduct > globalMax ? currentProduct : globalMax;
+            if (currentProduct == 0)
+                currentProduct = 1;
+        }
+
+        currentProduct = 1;
+        for(int i = nums.size() - 1; i >= 0; i--)
+        {
+            currentProduct *= nums[i];
+            globalMax = currentProduct > globalMax ? currentProduct : globalMax;
+            if (currentProduct == 0)
+                currentProduct = 1;
+        }
+
+        return globalMax;
+    }
+};
---
+++ Solutions/C++/Strings/ReverseWordsInString.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res = "";
+        int right = s.length() - 1;
+        while(right >= 0)
+        {
+	        //remove trailing white space
+            while(right >= 0 && s[right] == ' ')
+                right--;
+
+            if (right < 0)
+                break;
+
+            int left = right;
+            while (left >= 0 && s[left] != ' ')
+                left--;
+
+            if (res.length() > 0)
+                res += " ";
+            res += s.substr(left + 1, right - left);
+            right = left;
+        }
+
+        return res;
+    }
+};
